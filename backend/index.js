99444a0559a8bd7fe65112b8d99b32b025fbc08a
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2020-03-24 19:00:06 -0300

    aula 02

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,18 @@ const express = require('express');
 //Cria a aplicação
 const app = express();
 
+app.use(express.json());
+
+/**
+ * Tipos de parametros
+ * 
+ * Query Params:Parametros nomeados enviados na rota apos o '?',
+ * Filtros, paginação
+ * 
+ * Route Params: Parametros utilizados para identificar recursos
+ * 
+ * Request Body:Corpo da requesição utilizxado para criar ou alterar 
+ */
 app.get('/',(req,res)=>{
    return  res.json({
        event:'Semana Omnistack 11.0',
